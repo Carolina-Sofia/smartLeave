9b4f786b8925530c8e67c563affc87b3e9e37482
Carolina Sofia <c.sbernardes02@gmail.com>
2025-04-10 17:04:18 +0100

    Create function getWeekends - gives an array of weekends for a date interval

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,57 @@
 import { useState } from "react";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+function getWeekends() {
+  const today = new Date();
+  const end = new Date("2025-12-31");
+  const saturdays = [];
+  const sundays = [];
 
+  // saturdays are 6, sundays are 0 - this is getting the first saturday
+  while (today.getDay() !== 6) {
+    today.setDate(today.getDate() + 1);
+  }
+
+  // jumping 7 to 7 days to collect the saturdays
+  while (today <= end) {
+    const saturday = new Date(today);
+    const sunday = new Date(today);
+    sunday.setDate(sunday.getDate() + 1);
+
+    saturdays.push(saturday);
+    sundays.push(sunday);
+    today.setDate(today.getDate() + 7);
+  }
+
+  console.log("Today", today);
+  console.log("Saturdays:", saturdays);
+  console.log("Sundays:", sundays);
+
+  return;
+}
+
+function App() {
+  getWeekends();
   return (
     <>
-      <button className="btn btn-primary">Click me</button>
+      <h1 className="pb-1">Smart Leave</h1>
+      <p className="pb-5">We do the math. You book the flights.</p>
+      <div className="input-group mb-3">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Number of vacation days"
+          aria-label="Number of vacation days"
+          aria-describedby="button-addon2"
+        />
+        <button
+          className="btn btn-outline-secondary"
+          type="button"
+          id="button-addon2"
+        >
+          Submit
+        </button>
+      </div>
     </>
   );
 }
