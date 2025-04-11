8144e350da00eba8753ba48fe1707917358fff72
Carolina Sofia <c.sbernardes02@gmail.com>
2025-04-11 19:32:52 +0100

    Create function that sums number of holidays that are weekdays from a holiday.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import data from "./assets/holiday.json";
 
 function getWeekends() {
   const today = new Date();
@@ -12,7 +13,7 @@ function getWeekends() {
     today.setDate(today.getDate() + 1);
   }
 
-  // jumping 7 to 7 days to collect the saturdays
+  // jumping 7 to 7 days to collect the saturdays and sundays
   while (today <= end) {
     const saturday = new Date(today);
     const sunday = new Date(today);
@@ -23,15 +24,34 @@ function getWeekends() {
     today.setDate(today.getDate() + 7);
   }
 
-  console.log("Today", today);
-  console.log("Saturdays:", saturdays);
-  console.log("Sundays:", sundays);
+  // console.log("Today", today);
+  // console.log("Saturdays:", saturdays);
+  // console.log("Sundays:", sundays);
 
   return;
 }
 
+function getHolidays() {
+  const holidays = data.map((holiday) => holiday.date);
+  let sumHolidays = holidays.length;
+
+  // This substracts the weekends, so technicall we get 11 days off in portugal this year
+  for (let i = 0; i < holidays.length; i++) {
+    const holiday = new Date(holidays[i]);
+    const holidayDay = holiday.getDay();
+    if (holidayDay === 0) {
+      sumHolidays -= 1;
+    } else if (holidayDay === 6) {
+      sumHolidays -= 1;
+    }
+  }
+  console.log(holidays);
+  console.log("Number of weekday holidays", sumHolidays);
+}
+
 function App() {
   getWeekends();
+  getHolidays();
   return (
     <>
       <h1 className="pb-1">Smart Leave</h1>
